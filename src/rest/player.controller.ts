f6d1260af80e983d04a8b0a370b0de3908c8ff3f
itachi <enchantinggg4@gmail.com>
2020-10-28 05:16:42 +0300

    Leaderboard now includes players who played at least 1 ranked game this seaason

--- src/rest/player.controller.ts
+++ src/rest/player.controller.ts
@@ -6,6 +6,8 @@ import { LessThanOrEqual, Repository } from 'typeorm';
 import { Dota2Version } from 'gateway/shared-types/dota2version';
 import { VersionPlayer } from 'gameserver/entity/VersionPlayer';
 import { GameSeason } from 'gameserver/entity/GameSeason';
+import { LeaderboardEntryDto } from 'rest/dto/player.dto';
+import { MatchmakingMode } from 'gateway/shared-types/matchmaking-mode';
 
 @Controller('player')
 @ApiTags('player')
@@ -21,7 +23,9 @@ export class PlayerController {
   ) {}
 
   @Get('/:version/leaderboard')
-  async leaderboard(@Param('version') version: Dota2Version) {
+  async leaderboard(
+    @Param('version') version: Dota2Version,
+  ): Promise<LeaderboardEntryDto[]> {
     const currentSeason = await this.gameSeasonRepository.findOne({
       where: {
         start_timestamp: LessThanOrEqual(new Date()),
@@ -31,16 +35,20 @@ export class PlayerController {
       },
     });
 
+    const calibrationGames = 1;
+
     const leaderboard = await this.versionPlayerRepository.query(`
       select p.steam_id,
              p.mmr
       from version_player p
                left outer join player_in_match pim
                inner join match m on pim."matchId" = m.id
-                          on p.steam_id = pim."playerId" and m.timestamp > '${currentSeason.start_timestamp.toUTCString()}' and m.type = 0
+                          on p.steam_id = pim."playerId" and m.timestamp > '${currentSeason.start_timestamp.toUTCString()}' and m.type = ${
+      MatchmakingMode.RANKED
+    }
       
       group by p.steam_id, p.mmr
-      having count(pim) > 0
+      having count(pim) >= ${calibrationGames}
       order by p.mmr DESC;
 `);
 
