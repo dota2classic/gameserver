aeeec1680b56584aaf2acca6f63f033988df79cf
itachi <enchantinggg4@gmail.com>
2021-01-23 03:42:20 +0300

    Redis caches wIP

--- src/util/method-cache.ts
+++ src/util/method-cache.ts
@@ -1,30 +1,8 @@
-import { QueryCache } from 'util/query-cache';
+import { queryCacheFactory } from 'd2c-rcaches';
+import { REDIS_PASSWORD, REDIS_URL } from 'env';
 
-
-
-
-export function cached(ttl: number, uniqueKey: string) {
-  const cache = new QueryCache(ttl);
-
-
-  return function(
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor,
-  ) {
-    const originalMethod = descriptor.value;
-
-
-    descriptor.value = async function(...args) {
-      const cached = await cache.getCached([uniqueKey, args]);
-
-      if (cached === undefined) {
-        const result = await originalMethod.apply(this, args);
-
-        await cache.setNew([uniqueKey, args], result);
-        return result;
-      }
-      return cached;
-    };
-  };
-}
+export const cached = queryCacheFactory({
+  url: REDIS_URL(),
+  password: REDIS_PASSWORD(),
+  ttl: 0,
+});
