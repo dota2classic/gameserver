6aca2f8b69d3d32d06ecb364b56c5263f0bf63e0
itachi <enchantinggg4@gmail.com>
2021-02-06 23:28:48 +0300

    fixed double mmr bug

--- src/gameserver/event/match-recorded.event.ts
+++ src/gameserver/event/match-recorded.event.ts
@@ -1,5 +1,14 @@
-import { GameResultsEvent } from 'gateway/events/gs/game-results.event';
+import { GameResultsEvent, PlayerInMatchDTO } from 'gateway/events/gs/game-results.event';
+import { MatchmakingMode } from 'gateway/shared-types/matchmaking-mode';
 
-export class MatchRecordedEvent extends GameResultsEvent {
-
+export class MatchRecordedEvent {
+  constructor(
+    public readonly matchId: number,
+    public readonly radiantWin: boolean,
+    public readonly duration: number,
+    public readonly type: MatchmakingMode,
+    public readonly timestamp: number,
+    public readonly server: string,
+    public readonly players: PlayerInMatchDTO[],
+  ) {}
 }
