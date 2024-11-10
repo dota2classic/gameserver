3fb521571fb5b29e6b932197a92b1483a743d0e9
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-10 21:36:12 +0300

    Fix

--- src/gameserver/event-handler/match-failed.handler.ts
+++ src/gameserver/event-handler/match-failed.handler.ts
@@ -5,6 +5,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PlayerNotLoadedEvent } from 'gateway/events/bans/player-not-loaded.event';
 import { Logger } from '@nestjs/common';
+import { MatchEntity } from 'gameserver/model/match.entity';
+import { MatchmakingMode } from 'gateway/shared-types/matchmaking-mode';
 
 @EventsHandler(MatchFailedEvent)
 export class MatchFailedHandler implements IEventHandler<MatchFailedEvent> {
@@ -15,16 +17,28 @@ export class MatchFailedHandler implements IEventHandler<MatchFailedEvent> {
     private readonly playerCrimeLogEntityRepository: Repository<
       PlayerCrimeLogEntity
     >,
+    @InjectRepository(MatchEntity)
+    private readonly matchEntityRepository: Repository<MatchEntity>,
     private readonly ebus: EventBus,
   ) {}
 
   async handle(event: MatchFailedEvent) {
+    const match = await this.matchEntityRepository.findOne({
+      where: { id: event.matchId },
+    });
+    if (!match) {
+      this.logger.warn('MatchFailedEvent on non-existing match');
+      return;
+    }
     //
     this.logger.log(
-      `Players failed to load: ${event.failedPlayers
+      `Players failed to load into match ${match.id}: ${event.failedPlayers
         .map(it => it.value)
         .join(',')}`,
     );
+
+    if (match.matchInfoJson.mode === MatchmakingMode.BOTS) return;
+
     this.ebus.publishAll(
       event.failedPlayers.map(
         pl => new PlayerNotLoadedEvent(pl, event.matchId),
