4ac9a64ea786256f53e32c060d55bb4e1722f95d
Yuri Stepin <enchantinggg4@gmail.com>
2024-12-28 17:18:14 +0300

    Fix game mode skipping

--- src/gameserver/command/ProcessAchievements/process-achievements.handler.ts
+++ src/gameserver/command/ProcessAchievements/process-achievements.handler.ts
@@ -9,7 +9,6 @@ import { AchievementEntity } from 'gameserver/model/achievement.entity';
 import { combos } from 'util/cross';
 import { AchievementCompleteEvent } from 'gameserver/event/achievement-complete.event';
 import { AchievementService } from 'gameserver/achievement.service';
-import { MatchmakingMode } from 'gateway/shared-types/matchmaking-mode';
 
 @CommandHandler(ProcessAchievementsCommand)
 export class ProcessAchievementsHandler
@@ -32,12 +31,6 @@ export class ProcessAchievementsHandler
     const fm = await this.finishedMatchEntityRepository.findOne({
       where: { id: matchId },
     });
-
-    if (
-      matchmakingMode !== MatchmakingMode.RANKED &&
-      matchmakingMode !== MatchmakingMode.UNRANKED
-    )
-      return;
 
     const handles = combos(
       fm.players,
