1d14a03f0bb3edf809835407dc05d91e172c73f4
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-01 16:04:33 +0300

    fix

--- src/prometheus-guarded.controller.ts
+++ src/prometheus-guarded.controller.ts
@@ -1,14 +1,13 @@
 import { PrometheusController } from '@willsoto/nestjs-prometheus';
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
 
 @ApiExcludeController()
 @Controller()
 export class PrometheusGuardedController extends PrometheusController {
   @Get()
-  @UseGuards(AuthGuard("basic"))
+  // @UseGuards(AuthGuard("basic"))
   index(@Res({ passthrough: true }) response: Response) {
     return super.index(response);
   }
