fe2890f201b97822f3bec39d9d14ecc0e7211b23
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-12 21:02:23 +0300

    View based mmr bucket service instead of

--- src/@test/useFullModule.ts
+++ src/@test/useFullModule.ts
@@ -77,6 +77,22 @@ export function useFullModule(): TestEnvironment {
       .withPassword("password")
       .start();
 
+
+//     const client = new pg.Client(te.containers.pg.getConnectionUri());
+//     await client.connect();
+//     await client.query(`
+//         create or replace
+//         function fantasy_score(pim player_in_match) returns numeric
+// language plpgsql
+// as
+// $$
+// begin
+// return pim.kills * 0.3 + pim.deaths * -0.3 + pim.assists * 0.2 + pim.last_hits * 0.003 + pim.denies * 0.005 + pim.gpm * 0.002 + pim.xpm * 0.002 + pim.hero_healing * 0.01 + pim.hero_damage * 0.003 + pim.tower_damage * 0.01;
+// end;
+// $$;`);
+//     await client.end()
+
+
     te.containers.redis = await new RedisContainer()
       .withPassword("redispass")
       .start();
@@ -102,8 +118,8 @@ export function useFullModule(): TestEnvironment {
           username: te.containers.pg.getUsername(),
           password: te.containers.pg.getPassword(),
           entities: Entities,
-          synchronize: true,
-          dropSchema: true,
+          migrations: ["dist/src/database/migrations/*.*"],
+          migrationsRun: true,
           ssl: false,
         }),
         TypeOrmModule.forFeature(Entities),
