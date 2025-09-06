d5992f487fd22c91acb8c6682b4a5df1bf1c8a5c
Yuri Stepin <enchantinggg4@gmail.com>
2025-09-06 21:48:49 +0300

    Try this

--- src/rest/service/req-logging.interceptor.ts
+++ src/rest/service/req-logging.interceptor.ts
@@ -1,7 +1,7 @@
 // Import required modules
 import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { PATH_METADATA, SSE_METADATA } from '@nestjs/common/constants';
+import { finalize, Observable, tap } from 'rxjs';
+import { PATH_METADATA } from '@nestjs/common/constants';
 import * as path from 'path';
 import { Request, Response } from 'express';
 
@@ -9,16 +9,12 @@ import { Request, Response } from 'express';
 export class ReqLoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger("HTTP_REQUEST");
 
-  constructor() // @InjectMetric("http_requests_duration_seconds")
-  // public requestHistogram: Histogram<string>,
-  {}
+  constructor()  {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // Extract request and response objects
     const req: Request = context.switchToHttp().getRequest();
     const res: Response = context.switchToHttp().getResponse();
-
-    // req: FastifyRequest['raw'], res: FastifyReply['raw']
 
     const handler = context.getHandler();
     const controller = Reflect.getMetadata(PATH_METADATA, context.getClass());
@@ -27,27 +23,30 @@ export class ReqLoggingInterceptor implements NestInterceptor {
       controller,
       Reflect.getMetadata(PATH_METADATA, handler),
     );
+    //
+    // let d0 = performance.now();
+    //
+    // res.on("finish", () => {
+    //   const durationSeconds = (performance.now() - d0) / 1000;
+    //   this.logger.log(
+    //     `Request ${req.method} ${requestPath} took ${Math.round(durationSeconds * 1000)} ms`,
+    //   );
+    // });
 
-    const isSSE = Reflect.getMetadata(SSE_METADATA, handler);
+    const start = performance.now();
 
-    let d0 = performance.now();
 
-    res.on("finish", () => {
-      const durationSeconds = (performance.now() - d0) / 1000;
-
-      // console.log(
-      //   "Request finish, ",
-      //   req.method,
-      //   requestPath,
-      //   isSSE ? "sse" : "request",
-      //   res.statusCode.toString(),
-      //   durationSeconds,
-      // );
-      this.logger.log(
-        `Request ${req.method} ${requestPath} took ${Math.round(durationSeconds * 1000)} ms`,
-      );
-    });
-
-    return next.handle();
+    return next.handle().pipe(
+      tap(() => {
+        const handlerDone = performance.now();
+        this.logger.debug(`Handler took ${handlerDone - start} ms`);
+      }),
+      finalize(() => {
+        res.on('finish', () => {
+          const finish = performance.now();
+          this.logger.debug(`Total until res.finish: ${finish - start} ms`);
+        });
+      }),
+    );
   }
 }
