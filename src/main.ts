1581b5088d283b7f2ad0ac8969f40a6e3c2873ce
Yuri Stepin <enchantinggg4@gmail.com>
2025-10-20 17:53:56 +0300

    Fix

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,10 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CommandBus, EventBus, EventPublisher, ofType, QueryBus } from '@nestjs/cqrs';
+import { EventPublisher } from '@nestjs/cqrs';
 import { Transport } from '@nestjs/microservices';
-import { inspect } from 'util';
-import { INestApplication, Logger } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ServerActualizationRequestedEvent } from 'gateway/events/gs/server-actualization-requested.event';
-import { FindGameServerCommand } from 'gameserver/command/FindGameServer/find-game-server.command';
-import { ServerSessionSyncEvent } from 'gateway/events/gs/server-session-sync.event';
-import { LiveMatchUpdateEvent } from 'gateway/events/gs/live-match-update.event';
-import { GameServerDiscoveredEvent } from 'gateway/events/game-server-discovered.event';
-import { ServerStatusEvent } from 'gateway/events/gs/server-status.event';
 import './util/promise';
 import configuration from 'config/configuration';
 import { ConfigService } from '@nestjs/config';
@@ -63,7 +56,7 @@ async function bootstrap() {
       retryDelay: 5000,
       password: config.get("redis.password"),
     },
-  })
+  });
 
   // app.use(compression());
 
@@ -80,52 +73,6 @@ async function bootstrap() {
   await app.listen(5003, "0.0.0.0");
 
   await app.startAllMicroservices();
-
-  const publisher = app.get(EventPublisher);
-  prepareModels(publisher);
-
-  const ebus = app.get(EventBus);
-  const cbus = app.get(CommandBus);
-  const qbus = app.get(QueryBus);
-
-  const clogger = new Logger("CommandLogger");
-  const elogger = new Logger("EventLogger");
-
-  ebus.subscribe((e) => {
-    if (e.constructor.name === ServerActualizationRequestedEvent.name) return;
-    if (e.constructor.name === ServerSessionSyncEvent.name) return;
-    if (e.constructor.name === LiveMatchUpdateEvent.name) return;
-    if (e.constructor.name === GameServerDiscoveredEvent.name) return;
-    if (e.constructor.name === ServerStatusEvent.name) return;
-
-    elogger.log(`${inspect(e)}`);
-  });
-
-  // qbus.subscribe(e => {
-  //   qlogger.log(`${inspect(e)}`);
-  // });
-
-  cbus.pipe(ofType(FindGameServerCommand)).subscribe((e) => {
-    clogger.log(
-      `${inspect(e)}`,
-      // e.__proto__.constructor.name,
-    );
-  });
-
-  // const ms = app.get(MatchService);
-  //
-  // const [data, cnt] = await ms.getMatchPageFastest(0, 25, MatchmakingMode.UNRANKED);
-  // if(data.length !== 25){
-  //
-  //   console.error("Wrong", data.length)
-  // }
-
-  // const r1 = async () =>
-  //   (await app.get(RecordService).getMostKda()).map((it) => it.match.id);
-  // console.log(await r1());
-  //
-  // console.log(await r1());
-
   // for (let i = 0; i < 1; i++) {
   //   await app.get(GameServerService).generateFakeMatch("116514945");
   // }
