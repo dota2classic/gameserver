1d62c1e32e0b77005b8119ac24157e465e75d20c
Yuri Stepin <enchantinggg4@gmail.com>
2026-04-20 11:58:37 +0300

    Small fix

--- src/gameserver/event-handler/session-ended.handler.ts
+++ src/gameserver/event-handler/session-ended.handler.ts
@@ -1,12 +1,12 @@
-import { EventBus, EventsHandler, IEventHandler } from '@nestjs/cqrs';
-import { Logger } from '@nestjs/common';
-import { DataSource, Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { SessionEndedEvent } from 'gameserver/event/session-ended.event';
-import { GameServerSessionEntity } from 'gameserver/model/game-server-session.entity';
-import { GameSessionPlayerEntity } from 'gameserver/model/game-session-player.entity';
-import { MatchFinishedEvent } from 'gateway/events/match-finished.event';
-import { ReturnGoodPlayersToQueueEvent } from 'gateway/events/mm/return-good-players-to-queue.event';
+import { EventBus, EventsHandler, IEventHandler } from "@nestjs/cqrs";
+import { Logger } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { SessionEndedEvent } from "gameserver/event/session-ended.event";
+import { GameServerSessionEntity } from "gameserver/model/game-server-session.entity";
+import { GameSessionPlayerEntity } from "gameserver/model/game-session-player.entity";
+import { MatchFinishedEvent } from "gateway/events/match-finished.event";
+import { ReturnGoodPlayersToQueueEvent } from "gateway/events/mm/return-good-players-to-queue.event";
 
 @EventsHandler(SessionEndedEvent)
 export class SessionEndedHandler implements IEventHandler<SessionEndedEvent> {
@@ -40,9 +40,10 @@ export class SessionEndedHandler implements IEventHandler<SessionEndedEvent> {
       this.ebus.publish(
         new ReturnGoodPlayersToQueueEvent(goodSteamIds, session.matchId, session.matchmaking_mode),
       );
-    } else {
-      this.ebus.publish(new MatchFinishedEvent(session.matchId, session.asSummary()));
     }
+    this.ebus.publish(
+      new MatchFinishedEvent(session.matchId, session.asSummary()),
+    );
   }
 
   private async deleteSession(url: string): Promise<void> {
